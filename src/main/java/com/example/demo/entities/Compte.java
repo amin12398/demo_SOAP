e6fc77079076b795f7c4b63e83cc0b9e2b789c55
amin12398 <essaoudmedamine@gmail.com>
2024-12-10 13:22:46 +0100

    mycomm

---
+++ src/main/java/com/example/demo/entities/Compte.java
@@ -0,0 +1,31 @@
+package com.example.demo.entities;
+
+import com.example.demo.enums.TypeCompte;
+import jakarta.persistence.*;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@XmlRootElement // specifie that the class should be a xml
+@XmlAccessorType(XmlAccessType.FIELD) //map all fields of the class to xml elements
+public class Compte {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private double solde;
+    @Temporal(TemporalType.DATE)
+    private Date dateCreation;
+    @Enumerated(EnumType.STRING)
+    private TypeCompte type;
+
+
+}
